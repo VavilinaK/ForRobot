e2fbf20d6f4aa85e8b888cba384b1a7559f92993
Migunova Anastasia <78942134+VavilinaK@users.noreply.github.com>
2024-08-19 13:02:04 +0300

    Add files via upload

---
+++ StatusProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForRobot.Libr.Client
+{
+    public static class StatusProcess
+    {
+        public static readonly string None = "";
+
+        public static readonly string Error = "Ошибка!";
+
+        public static readonly string ErrorGeneration = "Ошибка генерации программы!";
+
+        public static readonly string RobotBusy = "Уже запущен процесс";
+
+        public static readonly string ProgrammReady = "Готов к работе";
+
+        public static readonly string ProgrammStart = "Программа запущена";
+
+        public static readonly string ProgrammPause = "Программа остановлена";
+
+        public static readonly string ProgrammStop = "Программа сброшена";
+
+        public static readonly string ProgrammEnd = "Программа завершена";
+
+        public static readonly string ProgrammNotSelected = "Не выбрана программа";
+    }
+}
