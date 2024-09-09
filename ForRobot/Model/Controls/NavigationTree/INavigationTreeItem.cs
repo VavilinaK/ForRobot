cdc2858cb6184ff490fc060919d28fed154aebc6
Мигунова_АМ <_@OP8>
2024-09-09 11:34:12 +0300

    09/09/24

---
+++ ForRobot/Model/Controls/NavigationTree/INavigationTreeItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Media.Imaging;
+using System.Collections.ObjectModel;
+
+namespace ForRobot.Model.Controls.NavigationTree
+{
+    public interface INavigationTreeItem : INotifyPropertyChanged
+    {
+        bool IsExpanded { get; set; }
+
+        bool IncludeFileChildren { get; }
+
+        /// <summary>
+        /// Наименование файла
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Иконка файла
+        /// </summary>
+        BitmapSource Icon { get; set; }
+
+        /// <summary>
+        /// Схема Диск/Папка/Файл
+        /// </summary>
+        string FullPathFile { get; set; }
+
+        ObservableCollection<INavigationTreeItem> Children { get; set; }
+
+        void DeleteChildren();
+    }
+}
