383812dc7091a7362e05af3c30f9aa411cbf0e46
Мигунова_АМ <_@OP8>
2024-09-26 16:10:48 +0300

    Добавила открытие схем детали при двойном клики

--- ForRobot/Views/Pages/Page2D.xaml.cs
+++ ForRobot/Views/Pages/Page2D.xaml.cs
@@ -1,11 +1,51 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Controls;
+
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
 
 using ForRobot.Model.Detals;
 
 namespace ForRobot.Views.Pages
 {
+    public partial class Image2D : System.Windows.Controls.Image
+    {
+        public static readonly RoutedEvent MouseDoubleClick = EventManager.RegisterRoutedEvent(nameof(MouseDoubleClickEvent), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Image2D));
+
+        public event RoutedEventHandler MouseDoubleClickEvent
+        {
+            add
+            {
+                AddHandler(MouseDoubleClick, value);
+            }
+            remove
+            {
+                RemoveHandler(MouseDoubleClick, value);
+            }
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                RaiseEvent(new MouseDoubleClickEventArgs(MouseDoubleClick, this));
+            }
+            base.OnMouseLeftButtonDown(e);
+        }
+
+        public class MouseDoubleClickEventArgs : RoutedEventArgs
+        {
+            public MouseDoubleClickEventArgs(RoutedEvent routedEvent, object source)
+                : base(routedEvent, source)
+            {
+            }
+        }
+
+    }
+
     /// <summary>
     /// Логика взаимодействия для Page2D.xaml
     /// </summary>
@@ -62,7 +102,25 @@ namespace ForRobot.Views.Pages
                 return _openImageCommand ??
                     (_openImageCommand = new RelayCommand(obj =>
                     {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create((obj as Image).Source as BitmapImage));
 
+                        string filePath = Path.Combine(Path.GetTempPath(), "Параметры_детали.png");
+
+                        using (var fileStream = new System.IO.FileStream(filePath, System.IO.FileMode.Create))
+                        {
+                            encoder.Save(fileStream);
+                        }
+
+                        ProcessStartInfo Info = new ProcessStartInfo()
+                        {
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            FileName = "explorer.exe",
+                            WindowStyle = ProcessWindowStyle.Normal,
+                            Arguments = filePath
+                        };
+                        Process.Start(Info);
                     }));
             }
         }
