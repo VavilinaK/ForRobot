4f4d1476995cb42fcb30ed98cd6efa540a8c261c
VavilinaK <migunova2003117@mail.ru>
2025-10-16 16:58:43 +0300

    16/10/2025

---
+++ ForRobot/Libr/Behavior/ClosedLayoutAnchorableBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.ComponentModel;
+
+using System.Windows.Interactivity;
+
+using AvalonDock.Layout;
+
+namespace ForRobot.Libr.Behavior
+{
+    /// <summary>
+    /// Поведение закрытия <see cref="AvalonDock.Layout.LayoutAnchorable"/> пин-кодом
+    /// </summary>
+    public class ClosedLayoutAnchorableBehavior : Behavior<LayoutAnchorable>
+    {
+        private LayoutAnchorable _layoutAnchorable = null;
+        private bool _isOpening = false;
+        
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this._layoutAnchorable = base.AssociatedObject;
+
+            if (this._layoutAnchorable != null)
+                this._layoutAnchorable.Hiding += HandleHidingEvent;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (this._layoutAnchorable != null)
+                this._layoutAnchorable.Hiding -= HandleHidingEvent;
+        }
+
+        private static void HandleHidingEvent(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        //private static void LayoutAnchorable_IsSelectedChanged(object sender, EventArgs e)
+        //{
+        //    if (sender is LayoutAnchorable layoutAnchorable)
+        //    {
+
+        //    }
+        //}
+    }
+}
